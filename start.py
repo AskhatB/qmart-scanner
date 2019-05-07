c3a337d6b05f52b490bc2e39b82fe5cf74cc1c00
Askhat <baltabaev.a2509@gmail.com>
2019-05-07 16:34:50 +0600

    fix: image uploading

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import pyzbar.pyzbar as pyzbar
 import base64
+import cv2
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 app = Flask(__name__)
@@ -11,7 +12,11 @@ def main():
     data = request.get_json()
     image = data['image']
     imgData = base64.b64decode(image)
-    decodedObjects = pyzbar.decode(imgData)
+    fileName = 'sample.jpg'
+    with open('sample.jpg', 'wb') as f:
+        f.write(imgData)
+    img = cv2.imread(fileName)
+    decodedObjects = pyzbar.decode(img)
     barcode = 'Not recognized'
     for object in decodedObjects:
         barcode = object.data.decode('utf-8')
@@ -19,4 +24,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False)
+    app.run()
